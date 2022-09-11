c6b80d383ca44398091ae6e076a72576f103683b
Piotrek M <pmarkiewicz@fessio.pl>
2022-09-11 13:32:06 +0200

    refactor: :recycle: split normalize_locales to smaller functions

--- pyi18n/tasks/normalize.py
+++ pyi18n/tasks/normalize.py
@@ -29,31 +29,49 @@ def normalize_locales(locale_path: str = "locales/") -> dict:
         print(f"[ERROR] {locale_path} is empty")
         exit(1)
 
+    __perform_normalize(locales, locale_path)
+
+
+def __perform_normalize(locales: Set[str], locale_path: str) -> None:
+    """ private method to perform normalization """
     for subclass in loaders.PyI18nBaseLoader.__subclasses__():
         if subclass.__name__ == "PyI18nXMLLoader" \
          and environ["PYI18N_TEST_ENV"]:
             continue
+
         loader: loaders.PyI18nBaseLoader = subclass(locale_path)
 
         content: Dict[str] = loader.load(locales)
 
         sorted_content: Dict[Any] = __sort_nested(content)
 
-        ext: str = loader.type().replace('yaml', 'yml')
-        dumper: str = {
-            "json": lambda x, y: json_dump(x, y, indent=4, sort_keys=True),
-            "yml": yaml_dump,
-            "xml": lambda x, _: xml_dump(x, pretty=True)
-        }
+        __save_normalized(locales, loader,
+            locale_path, sorted_content)
 
-        for locale in locales:
-            file_path: str = f"{locale_path}{locale}.{ext}"
-            with open(file_path, "w", encoding="utf-8") as _f:
-                dumper[ext]({locale: sorted_content[locale]}, _f)
+
+def __save_normalized(
+        locales: Set[str],
+        loader: loaders.PyI18nBaseLoader,
+        locale_path: str,
+        sorted_content: Dict[Any]
+    ) -> None:
+    """ private function to save the normalized content """
+
+    ext: str = loader.type().replace('yaml', 'yml')
+    dumper: str = {
+        "json": lambda x, y: json_dump(x, y, indent=4, sort_keys=True),
+        "yml": yaml_dump,
+        "xml": lambda x, _: xml_dump(x, pretty=True)
+    }
+
+    for locale in locales:
+        file_path: str = f"{locale_path}{locale}.{ext}"
+        with open(file_path, "w", encoding="utf-8") as _f:
+            dumper[ext]({locale: sorted_content[locale]}, _f)
 
 
 def __sort_nested(dictionary: dict) -> dict:
-    """ Sorts nested dictionaries """
+    """ private function to sort nested dictionaries """
     dictionary: dict = dictionary
     for d in dictionary:
         dictionary[d] = dict(sorted(dictionary[d].items()))
