f4e96f00279ce80ef95fd7cfb33b6eeac17bff1c
Piotrek M <pmarkiewicz@fessio.pl>
2023-03-18 18:31:47 +0100

    refactor: fix load_locale complexity

--- pyi18n/helpers.py
+++ pyi18n/helpers.py
@@ -4,10 +4,13 @@ utilities that can be used throughout the application.
 
 Functions:
 - load_locale: Load translations from a single locale directory.
+- get_files: Get a list of files in a directory with a given file extension.
+- load_file: Load translations from a single file
 """
 
 from os import listdir, stat
 from os.path import exists, join, splitext
+from typing import List
 from yaml import FullLoader
 
 
@@ -22,29 +25,50 @@ def load_locale(path: str, ser_mod: object, l_type: str) -> dict:
         dict: loaded translations for the locale
     """
     if not exists(path):
-        print("f[WARNING] path {path} doesn't exist, probably you forgot",
+        print(f"[WARNING] path {path} doesn't exist, probably you forgot",
               "to add it to the available locales list.")
         return {}
-
-    loader_params: object = {"Loader": FullLoader} if l_type == "yaml" else {}
 
     file_extension: str = ser_mod.__name__.replace('yaml', 'yml')
 
     loaded_locale: dict = {}
-    for file_name in listdir(path):
-        if not file_name.endswith(file_extension):
-            continue
-
+    for file_name in get_files(path, file_extension):
         file_path: str = join(path, file_name)
         namespace: str = splitext(file_name)[0]
 
-        # Skip empty files
         if not stat(file_path).st_size:
             continue
 
-        with open(file_path, 'r', encoding='utf-8') as file:
-            locale_content: dict = ser_mod.load(file, **loader_params)
-
-            loaded_locale[namespace] = locale_content
+        locale_content = load_file(file_path, ser_mod, l_type)
+        loaded_locale[namespace] = locale_content
 
     return loaded_locale
+
+
+def get_files(path: str, file_extension: str) -> List[str]:
+    """Get a list of files in a directory with a given file extension.
+
+    Args:
+        path (str): path to the directory
+        file_extension (str): file extension to search for (e.g. ".yml")
+
+    Returns:
+        List[str]: list of file names in the directory with the given extension
+    """
+    return [file_name for file_name in listdir(path) if file_name.endswith(file_extension)]
+
+
+def load_file(file_path: str, ser_mod: object, l_type: str) -> dict:
+    """Load translations from a single file.
+
+    Args:
+        file_path (str): path to the translation file
+        ser_mod (object): module for serialization
+        l_type (str): type of file to load (e.g. "yaml", "json")
+
+    Returns:
+        dict: loaded translations from the file
+    """
+    loader_params: dict = {"Loader": FullLoader} if l_type == "yaml" else {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return ser_mod.load(file, **loader_params)
